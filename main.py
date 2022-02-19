7b69e42934aeb6339ca3d90ceda818fdb89b8960
Johnny <JohnnyLieu@yahoo.com>
2022-02-19 14:17:54 -0800

    able to open and print header of csv file

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import csv
 import sys
 
+with open("metadata.csv", 'r') as metafile:
+    reader = csv.reader(metafile, delimiter=",")
+    header = next(reader)
+    print(header)
 
 
 # python main.py path_to_input_csv path_to_output_csv min_dist max_dist
