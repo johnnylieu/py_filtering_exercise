cdf98a91b70c2e5711b28901a2c05721817466c0
Johnny <JohnnyLieu@yahoo.com>
2022-02-19 14:24:56 -0800

    able to open csv and print min and max distance of csv

--- main.py
+++ main.py
@@ -4,7 +4,12 @@ import sys
 with open("metadata.csv", 'r') as metafile:
     reader = csv.reader(metafile, delimiter=",")
     header = next(reader)
-    print(header)
+    # print(header) # works
+    for row in reader:
+        minDis = row[5]
+        maxDis = row[6]
+        # print(minDis, maxDis) # works
+
 
 
 # python main.py path_to_input_csv path_to_output_csv min_dist max_dist
