877573a384260610f36f90b3810ddb9bf589eaa5
Johnny <JohnnyLieu@yahoo.com>
2022-02-22 14:38:58 -0800

    created header class, constructor and headerOutput function to out put the top header row

--- task 3/csv_distance_filter.py
+++ task 3/csv_distance_filter.py
@@ -2,59 +2,75 @@ import csv
 import sys
 import timeit
 
-class CsvFiltering:
-    def beginFiltering():
-        output = open(sys.argv[2], 'w')
+class header:
+    def __init__(self, inputFile, outputFile):
+        self.inputFile = inputFile
+        self.outputFile = outputFile
+
+    def headerOutput(self):
+        output = open(self.outputFile, 'w')
         output_header = "Date, Time, File, x, y, Theta, Distance\n" # optional (not part of task) but looks cleaner
+        # print(output_header) #works
         output.write(output_header)
 
-        file = open(sys.argv[1])
-        reader = csv.reader(file)
-        rowsOriginal = len(list(reader))
+# class rows:
+#     def __init__(self, inputFile, min, max):
+#         self.inputFile = inputFile
+#         self.min = min
+#         self.max = max
 
-        minDis = int(sys.argv[3])
-        maxDis = int(sys.argv[4])
-        # print(minDis, maxDis) # works
+#     def countRows(mainFile, min, max)
+#         file = open(mainFile)
+#         reader = csv.reader(file)
+#         rowsOriginal = len(list(reader))
 
-        with open(sys.argv[1], 'r') as metafile:
-            reader = csv.reader(metafile, delimiter=",")
-            # header = next(reader)
-            # print(header) # works
-            for row in reader:
-                date = row[0]
-                time = row[1]
-                file = row[2]
-                x = row[3]
-                y = row[4]
-                theta = row[5]
-                distance = int(row[6])
-                # print(row) # works
-                line = "{},{},{},{},{},{},{}\n".format(date, time, file, x, y , theta, distance) # output will be in this format for each line
-                if distance >= minDis:
-                    if distance <= maxDis:
-                        output.write(line) # output of new file will write this line everytime we iterate through a row
-        output.close()
+#         minDis = int(sys.argv[3])
+#         maxDis = int(sys.argv[4])
+#         # print(minDis, maxDis) # works
 
-        file = open(sys.argv[2])
-        reader = csv.reader(file)
-        rowsNew = len(list(reader))
-        # print(rowsOriginal) # works
-        # print(rowsNew) # works
-        rowsDiscarded = rowsOriginal - rowsNew - 1 # - 1 to account for the header that I added
-        percent = (rowsNew / rowsOriginal) * 100
-        # print(percent) #works
-        # print(rowsDiscarded) # works
-        print(f"\n{sys.argv[1]} has {rowsOriginal} rows") #works
-        print(f"{sys.argv[2]} has {rowsNew} rows") #works
-        print(f"There were {rowsDiscarded} rows discarded")
-        print(f"There are {percent}% of images within range\n")
-        file.close()
-        metafile.close()
+#         with open(sys.argv[1], 'r') as metafile:
+#             reader = csv.reader(metafile, delimiter=",")
+#             # header = next(reader)
+#             # print(header) # works
+#             for row in reader:
+#                 date = row[0]
+#                 time = row[1]
+#                 file = row[2]
+#                 x = row[3]
+#                 y = row[4]
+#                 theta = row[5]
+#                 distance = int(row[6])
+#                 # print(row) # works
+#                 line = "{},{},{},{},{},{},{}\n".format(date, time, file, x, y , theta, distance) # output will be in this format for each line
+#                 if distance >= minDis:
+#                     if distance <= maxDis:
+#                         output.write(line) # output of new file will write this line everytime we iterate through a row
+#         output.close()
 
-        print(f"Execution time is: {timeit.timeit()}\n")
+#         file = open(sys.argv[2])
+#         reader = csv.reader(file)
+#         rowsNew = len(list(reader))
+#         # print(rowsOriginal) # works
+#         # print(rowsNew) # works
+#         rowsDiscarded = rowsOriginal - rowsNew - 1 # - 1 to account for the header that I added
+#         percent = (rowsNew / rowsOriginal) * 100
+#         # print(percent) #works
+#         # print(rowsDiscarded) # works
+#         print(f"\n{sys.argv[1]} has {rowsOriginal} rows") #works
+#         print(f"{sys.argv[2]} has {rowsNew} rows") #works
+#         print(f"There were {rowsDiscarded} rows discarded")
+#         print(f"There are {percent}% of images within range\n")
+#         file.close()
+#         metafile.close()
+
+#         print(f"Execution time is: {timeit.timeit()}\n")
 
 def main():
-    CsvFiltering.beginFiltering()
+    currentHeader = header(sys.argv[1], sys.argv[2])
+    currentHeader.headerOutput()
+
+if __name__ == "__main__":
+    main()
 
 # PS: Lines 3 and 4 of main.py could have been left at the bottom of this file (see code below) but I just thought importing it was cooler ¯\_(ツ)_/¯
 # if __name__ == "__main__":
--- task 3/main.py
+++ task 3/main.py
@@ -1,4 +1,4 @@
 from csv_distance_filter import main
 
 if __name__ == "__main__":
-    main()
+    main(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
