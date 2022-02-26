448a3952c0ceabeff8aa7dbc9b10a064f2fdb440
Johnny <JohnnyLieu@yahoo.com>
2022-02-26 13:49:03 -0800

    shortening code to be more clear and concise

--- main.py
+++ main.py
@@ -1,55 +1,29 @@
 import csv
 import sys
-import timeit
+import time
 
-output = open(sys.argv[2], 'w')
-output_header = "Date, Time, File, x, y, Theta, Distance\n" # optional (not part of task) but looks cleaner
-output.write(output_header)
+t0 = time.time()
 
-file = open(sys.argv[1])
-reader = csv.reader(file)
-rowsOriginal = len(list(reader))
-
-minDis = int(sys.argv[3])
-maxDis = int(sys.argv[4])
-# print(minDis, maxDis) # works
+input = sys.argv[1]
+output = sys.argv[2]
+min_dis = sys.argv[3]
+max_dis = sys.argv[4]
 
 with open(sys.argv[1], 'r') as metafile:
     reader = csv.reader(metafile, delimiter=",")
-    # header = next(reader)
-    # print(header) # works
     for row in reader:
-        date = row[0]
-        time = row[1]
-        file = row[2]
-        x = row[3]
-        y = row[4]
-        theta = row[5]
-        distance = int(row[6])
-        # print(row) # works
-        line = "{},{},{},{},{},{},{}\n".format(date, time, file, x, y , theta, distance) # output will be in this format for each line
-        if distance >= minDis:
-            if distance <= maxDis:
-                output.write(line) # output of new file will write this line everytime we iterate through a row
-output.close()
+        distance = int(row[-1])
+            with open(sys.argv[2], 'x') as output_file: # using x instead of w returns an error if file already exists
+                lines = metafile.readlines()
+                total_lines = len(lines)
+                output_line = 0
 
-file = open(sys.argv[2])
-reader = csv.reader(file)
-rowsNew = len(list(reader))
-# print(rowsOriginal) # works
-# print(rowsNew) # works
-rowsDiscarded = rowsOriginal - rowsNew - 1 # - 1 to account for the header that I added
-percent = (rowsNew / rowsOriginal) * 100
-# print(percent) #works
-# print(rowsDiscarded) # works
-print(f"\n{sys.argv[1]} has {rowsOriginal} rows") #works
-print(f"{sys.argv[2]} has {rowsNew} rows") #works
-print(f"There were {rowsDiscarded} rows discarded")
-print(f"There are {percent}% of images within range\n")
-file.close()
-metafile.close()
+                if distance >= min_dis and distance <= max_dis:
+                    output_file.write(lines)
 
-print(f"Execution time is: {timeit.timeit()}\n")
+t1 = time.time()
+
+total_time = t1 - t0
 
 # task 2
 # # of total rows
